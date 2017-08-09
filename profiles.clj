73826d289062411d2ab1d040ce8b3aae0b98183c
Sam O'Leary <ssjoleary@gmail.com>
2017-08-09 23:09:30 +0100

    Add lein profiles.clj

---
+++ profiles.clj
@@ -0,0 +1,7 @@
+{:user {:dependencies [[cljfmt  "0.5.1"]]
+        :plugins      [[lein-cljfmt      "0.5.7"]
+                       [jonase/eastwood  "0.2.4"]
+                       [lein-kibit       "0.1.5"]
+                       [lein-bikeshed    "0.4.1"]
+                       [venantius/ultra  "0.5.1"]]
+        :repl-options {:init (require 'cljfmt.core)}}}
